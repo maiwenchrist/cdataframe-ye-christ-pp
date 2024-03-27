726e8e18a401ab77809e5433f281f7d52d150f7c
maiwenchrist <maiwen.christ@efrei.net>
2024-03-27 16:11:14 +0100

    Commencement de la première fonction COLUMN*create_column(char* title)

--- function.h
+++ function.h
@@ -1,15 +1,6 @@
-//
-<<<<<<< HEAD
-// Created by yeemm on 27/03/2024.
-=======
-// Created by maiwe on 27/03/2024.
->>>>>>> 88e6451 (Commencement de la première fonction COLUMN*create_column(char* title))
-//
-
 #ifndef CDATAFRAME_YE_CHRIST_PP_FUNCTION_H
 #define CDATAFRAME_YE_CHRIST_PP_FUNCTION_H
-<<<<<<< HEAD
-=======
+
 #define REALOC_SIZE 256
 
 typedef struct{
@@ -21,6 +12,5 @@ typedef struct{
 
 COLUMN *create_column(char* title);
 
->>>>>>> 88e6451 (Commencement de la première fonction COLUMN*create_column(char* title))
 
 #endif //CDATAFRAME_YE_CHRIST_PP_FUNCTION_H
